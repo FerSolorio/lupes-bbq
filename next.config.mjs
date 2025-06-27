6267e23dbebf72a0695dc2d215645f1f860dba78
FerSolorio <elia.fsolorio@gmail.com>
2025-06-27 14:09:26 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,33 @@
 /** @type {import('next').NextConfig} */
+
+// Define el nombre de tu repositorio aquí
+const repo = 'lupes-bbq'
+
+// Configura el prefijo de assets y la ruta base para producción
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 const nextConfig = {
+  // Habilita la exportación estática
   output: 'export',
-  trailingSlash: true,
-  distDir: 'out',
+  
+  // Configura las rutas solo para el despliegue en producción (GitHub Pages)
+  assetPrefix: process.env.NODE_ENV === 'production' ? assetPrefix : undefined,
+  basePath: process.env.NODE_ENV === 'production' ? basePath : undefined,
+  
+  // Necesario para que las imágenes funcionen en la exportación estática
+  images: {
+    unoptimized: true,
+  },
+  
+  // Opciones para ignorar errores de lint y typescript durante la construcción
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true
-  },
-  // Remover basePath y assetPrefix para simplificar
 }
 
 export default nextConfig
+
